00a3d14e35954bcdf03211b45ee779ed13f13e7d
LouAnne <louanne.m0122@gmail.com>
2021-07-29 16:50:11 +0200

    Simplification des fonctions

--- src/utiles/filters.js
+++ src/utiles/filters.js
@@ -9,140 +9,72 @@ export default function filterTags() {
         for(let i=0; i<response.photographers.length;i++){
             //Portrait
             let filterPortrait = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("portrait")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("portrait")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('portrait') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('portrait') || (document.getElementById(response.photographers[i].id).style.display = 'none')   
             } 
-
-            //Event listener
-            document.querySelectorAll('.portrait').forEach(btn => {
-            btn.addEventListener('click', filterPortrait);
-        });
+            document.querySelectorAll('.portrait').forEach(btn => { btn.addEventListener('click', filterPortrait);});
 
 
             //Art
             let filterArt = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("art")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("art")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('art') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('art') || (document.getElementById(response.photographers[i].id).style.display = 'none')       
             } 
-
-            //Event listener
-            document.querySelectorAll('.art').forEach(btn => {
-            btn.addEventListener('click',filterArt);
-            });
+            document.querySelectorAll('.art').forEach(btn => { btn.addEventListener('click',filterArt);});
 
 
 
             //Fashion
             let filterFashion = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("fashion")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("fashion")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('fashion') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('fashion') || (document.getElementById(response.photographers[i].id).style.display = 'none')           
             } 
-
-            //Event listener
-            document.querySelectorAll('.fashion').forEach(btn => {
-            btn.addEventListener('click',filterFashion);
-            });
+            document.querySelectorAll('.fashion').forEach(btn => { btn.addEventListener('click',filterFashion); });
 
 
-            //Fashion
+
+            //Architecture
             let filterArchitecture = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("architecture")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("architecture")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('architecture') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('architecture') || (document.getElementById(response.photographers[i].id).style.display = 'none')         
             } 
-
-            //Event listener
-            document.querySelectorAll('.architecture').forEach(btn => {
-            btn.addEventListener('click',filterArchitecture);
-            });
+            document.querySelectorAll('.architecture').forEach(btn => { btn.addEventListener('click',filterArchitecture);});
 
 
 
             //Travel
             let filterTravel = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("travel")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("travel")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('travel') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('travel') || (document.getElementById(response.photographers[i].id).style.display = 'none')           
             } 
-
-            //Event listener
-            document.querySelectorAll('.travel').forEach(btn => {
-            btn.addEventListener('click',filterTravel);
-            });
+            document.querySelectorAll('.travel').forEach(btn => { btn.addEventListener('click',filterTravel);});
 
 
 
             //Sport
             let filterSport = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("sports")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("sports")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('sports') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('sports') || (document.getElementById(response.photographers[i].id).style.display = 'none')          
             } 
-
-            //Event listener
-            document.querySelectorAll('.sports').forEach(btn => {
-            btn.addEventListener('click',filterSport);
-            });
+            document.querySelectorAll('.sports').forEach(btn => { btn.addEventListener('click',filterSport);});
 
 
 
             //Animals
             let filterAnimals = (e) => {
-                //Fonction ici
                 e.preventDefault();
                 response.photographers[i].tags.includes('animals') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('animals') || (document.getElementById(response.photographers[i].id).style.display = 'none')   
             } 
-
-            //Event listener
-            document.querySelectorAll('.animals').forEach(btn => {
-            btn.addEventListener('click',filterAnimals);
-            });
+            document.querySelectorAll('.animals').forEach(btn => { btn.addEventListener('click',filterAnimals); });
 
 
 
             //Events
             let filterEvents = (e) => {
-                //Fonction ici
                 e.preventDefault();
-                if(response.photographers[i].tags.includes("events")) {
-                    document.getElementById(response.photographers[i].id).style.display = "flex";
-                } else if (!response.photographers[i].tags.includes("events")){
-                    document.getElementById(response.photographers[i].id).style.display = "none";
-                }     
+                response.photographers[i].tags.includes('events') ? document.getElementById(response.photographers[i].id).style.display = 'flex' : response.photographers[i].tags.includes('events') || (document.getElementById(response.photographers[i].id).style.display = 'none')        
             } 
-
-            //Event listener
-            document.querySelectorAll('.events').forEach(btn => {
-            btn.addEventListener('click',filterEvents);
-            });
-
+            document.querySelectorAll('.events').forEach(btn => { btn.addEventListener('click',filterEvents); });
         }
     });
 
