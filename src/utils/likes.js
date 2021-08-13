3bc02bc624b21ab8a9971987de38466ee895f628
louannem <84766775+louannem@users.noreply.github.com>
2021-08-13 14:36:46 +0200

    Fonction Like v0.5 (incrément)

--- src/utils/likes.js
+++ src/utils/likes.js
@@ -5,28 +5,41 @@ export default function likeFunction() {
     let numberArray = [];
     let likesSum = 0;
 
+
+    //Récupère le paramètre
+    const queryString  = window.location.search;
+    //Réucpère l'id
+    const urlParam = new URLSearchParams(queryString);
+    const urlID = urlParam.get('id')
+
     fetchData()
     .then(response => {
         for(let i=0; i < response.media.length; i++) {
-            let likeID = document.getElementById(response.media[i].id);
-            console.log(likeID) 
+            if(response.media[i].photographerId == urlID) {
+
+                //Identifie les bloc de like
+                let likeID = document.getElementById(response.media[i].id);
+                let iconBtn = likeID.nextElementSibling;
+
+                //Fonction pour incrémenter le like
+                if(likeID = response.media[i].id) { 
+                    //Récupère la valeur des likes
+                    let likeNumber = document.getElementById(response.media[i].id).textContent;
+                    likeNumber = Number(likeNumber);
+ 
+                    let addLike = () => {
+                        likeNumber += 1;
+                        document.getElementById(response.media[i].id).innerText = likeNumber;
+                    }
+                    iconBtn.addEventListener('click', addLike) 
+                }
+            }
         }
     })
-    
-    let addLike = () => {
-        let likeNumber;
-        for(let i = 0; i < likesNumbers.length; i++) {
-            likeNumber = Number(likesNumbers[i].textContent);
-        }
-    }
-    let iconBtn = document.getElementsByClassName('likes__icon');
 
-    for(let i = 0; i < iconBtn.length; i++) {
-        iconBtn[i].addEventListener('click', addLike)
-    }
 
+    //for(let i = 0; i < iconBtn.length; i++) {  iconBtn[i].addEventListener('click', addLike)   }
     likesNumbers.forEach(element => { numberArray.push(element.textContent); })
-
     for(let i= 0; i < numberArray.length; i++) {  likesSum += Number(numberArray[i]);   }
     document.getElementById('likes__total').innerText = likesSum; 
 }
